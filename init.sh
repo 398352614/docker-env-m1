d4b8fdacaeb5f41b3ec49c761e8c01b518886efb
398352614 <398352614@qq.com>
2022-07-01 10:58:03 +0800

    1.0

--- init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-docker exec -i mysql bash -c "mysql -uroot -p123456 -e \"
-use mysql;
-update user set host='%' where user='root';
-flush privileges;
-grant all privileges on *.* to 'root'@'%';
-flush privileges;\"
-"
