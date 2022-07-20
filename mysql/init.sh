08a0cf54d1fe84c1dd6fefb7633d87c344c3ff7f
398352614 <398352614@qq.com>
2022-07-20 11:46:01 +0800

    1.2

--- mysql/init.sh
+++ mysql/init.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-mysql -uroot -p123456 -e "use mysql;
+source ./.env
+mysql -uroot -p${MYSQL_PASSWORD} -e "use mysql;
 update user set host='%' where user='root';
 flush privileges;
 grant all privileges on *.* to 'root'@'%';
